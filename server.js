8a62ebafee2fe14146f7eb6b5aa866668e280a9a
Alan Cota <alancota@COTAL01MBP.admin.cotahome.net>
2018-01-19 14:27:01 -0600

    Added HTTPS

--- server.js
+++ server.js
@@ -2,11 +2,22 @@
 console.log("Starting the server...");
 var os = require('os');
 const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const app = require('./app');
 
-const port = process.env.SERVER_PORT || 8001;
+// Port configuration
+const port = process.env.HTTP_SERVER_PORT || 8001;
+const portSsl = process.env.HTTPS_SERVER_PORT || 8002;
+
 const server = http.createServer(app);
+const serverSsl = https.createServer({
+    key: fs.readFileSync('./certs/key.pem'),
+    cert: fs.readFileSync('./certs/cert.pem')
+},app);
 
 server.listen(port);
+serverSsl.listen(portSsl);
 
-console.log("Server running at: http://"+os.hostname+":"+port);
+console.log("Server HTTP running at: http://"+os.hostname+":"+port);
+console.log("Server HTTPS running at: https://"+os.hostname+":"+portSsl);
