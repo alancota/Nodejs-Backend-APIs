093e5abe7bfcfb3a26dc09761e94128896747165
Alan Cota <alancota@COTAL01MBP-5.admin.cotahome.net>
2018-07-11 09:43:20 -0500

    Added new whitelist IPs

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const delay = require('express-delay');
 
 // Whitelist the following IPs
-var ips = ['34.213.86.33', '34.211.97.178'];
+var ips = ['34.213.86.33', '34.211.97.178', '54.202.110.87', 'ip-172-31-1-4.us-west-2.compute.internal'];
 
 const app = express();
 
