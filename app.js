524abea99134d6f767e9bc036b126e1d0663a47a
Alan Cota <alancota@COTAL01MBP-5.admin.cotahome.net>
2018-07-11 10:17:22 -0500

    New IP

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const delay = require('express-delay');
 
 // Whitelist the following IPs
-var ips = ['34.213.86.33', '34.211.97.178', '54.202.110.87', 'ip-172-31-1-4.us-west-2.compute.internal'];
+var ips = ['34.213.86.33', '34.211.97.178', '54.202.110.87', 'ip-172-31-1-4.us-west-2.compute.internal', '35.161.52.131'];
 
 const app = express();
 
